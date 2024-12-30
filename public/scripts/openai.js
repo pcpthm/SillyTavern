f9091128f1ad38fcd5268b6bb0b5ea50e5df68fd
pcpthm <pcpthm@gmail.com>
2024-12-30 13:46:38 +0900

    Fix for Mistral parameter restriction

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -2183,6 +2183,10 @@ async function sendOpenAIRequest(type, messages, signal) {
     }
 
     if (isMistral) {
+        if (generate_data['temperature'] === 0) {
+            generate_data['top_p'] = 1;
+        }
+
         generate_data['safe_prompt'] = false; // already defaults to false, but just incase they change that in the future.
         generate_data['stop'] = getCustomStoppingStrings(); // Mistral shouldn't have limits on stop strings.
     }
